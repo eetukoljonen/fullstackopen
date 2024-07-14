6b57f3ddc9a14240898ca41a2ed325726a944754
Eetu Koljonen <eetu.koljonen@gmail.com>
2024-07-14 16:08:28 +0300

    2.19 done

--- part2/dataforcountries/src/App.jsx
+++ part2/dataforcountries/src/App.jsx
@@ -6,6 +6,7 @@ import Countries from './components/Countries'
 function App() {
 	const [filter, setFilter] = useState('')
 	const [countries, setCountries] = useState([])
+	const [country, setCountry] = useState(null)
 
 	useEffect(() => {
 		countryService
@@ -17,6 +18,11 @@ function App() {
 
 	const handleFilterChange = (event) => {
 		setFilter(event.target.value)
+		setCountry(null)
+	}
+
+	const handleShowClick = (country) => {
+		setCountry([country])
 	}
 
 	const filtered = filter
@@ -25,7 +31,7 @@ function App() {
 	return (
 		<div>
 			<Filter filter={filter} handleFilterChange={handleFilterChange} />
-			<Countries countries={filtered}/>
+			<Countries handleShowClick={handleShowClick} countries={country ? country : filtered}/>
 		</div>
 	)
 }
--- part2/dataforcountries/src/components/Countries.jsx
+++ part2/dataforcountries/src/components/Countries.jsx
@@ -1,24 +1,28 @@
 
-const Countries = ({countries}) => {
+const Country = ({country}) => (
+	<div>
+		<h1>{country.name.common}</h1>
+		<p>capital: {country.capital}</p>
+		<p>area: {country.area}</p>
+		<h3>languages:</h3>
+		<ul>
+			{Object.values(country.languages).map((language) => (
+				<li key={language}>{language}</li>
+			))}
+		</ul>
+		<div>
+			<img src={country.flags.png}/>
+		</div>
+	</div>
+)
+
+const Countries = ({countries, handleShowClick}) => {
 	if (countries.length > 10)
 		return (<p>Too many matches, specify another filter</p>)
 	else if (countries.length === 1) {
 		const country = countries[0]
 		return (
-			<div>
-				<h1>{country.name.common}</h1>
-				<p>capital: {country.capital}</p>
-				<p>area: {country.area}</p>
-				<h3>languages:</h3>
-				<ul>
-					{Object.values(country.languages).map((language) => (
-						<li key={language}>{language}</li>
-					))}
-				</ul>
-				<div>
-					<img src={country.flags.png}/>
-				</div>
-			</div>
+			<Country country={country} />
 		)
 	}
 	return (
@@ -26,6 +30,7 @@ const Countries = ({countries}) => {
 			{countries.map(country => 
 				<p
 					key={country.name.common}>{country.name.common}
+					<button onClick={() => handleShowClick(country)}>show</button>
 				</p>
 			)}
 		</>
