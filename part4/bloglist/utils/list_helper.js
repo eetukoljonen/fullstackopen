42153d4525bc76257ebba3175f303ad430b55dff
Eetu Koljonen <eetu.koljonen@gmail.com>
2024-07-18 18:50:08 +0300

    4.5 done

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,7 +2,18 @@ const dummy = (blogs) => 1
 
 const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum + blog.likes, 0)
 
+const favoriteBlog = (blogs) => {
+  const blog = blogs.reduce((maxLikeBlog, blog) => {
+    if (!maxLikeBlog)
+      return blog
+    return maxLikeBlog.likes < blog.likes ? blog : maxLikeBlog
+  }, null)
+  console.log(blog)
+  return blog
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
