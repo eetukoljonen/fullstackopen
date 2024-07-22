db52f8ce25e42d60a76f342324ba7da8ffb9ffde
Eetu Koljonen <eetu.koljonen@gmail.com>
2024-07-22 19:56:04 +0300

    4.20 done

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -32,4 +32,12 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
-module.exports = { requestLogger, errorHandler, unknownEndpoint }
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '')
+  }
+  next()
+}
+
+module.exports = { requestLogger, errorHandler, unknownEndpoint, tokenExtractor }
