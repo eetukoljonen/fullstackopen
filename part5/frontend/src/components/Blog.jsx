28bf052f597591340b2533a0aa0a2a02c266d993
Eetu Koljonen <eetu.koljonen@gmail.com>
2024-07-25 16:53:33 +0300

    5.6 & 5.7 done

--- part5/frontend/src/components/Blog.jsx
+++ part5/frontend/src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const buttonLabel = visible ? 'hide' : 'view'
+
+  const showRest = () => (
+    <>
+      {blog.url}  <br />
+      likes {blog.likes} <button>like</button> <br />
+      {blog.author}
+    </>
+  )
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} <button onClick={() => setVisible(!visible)}>{buttonLabel}</button>  <br />
+        {visible && showRest()}
+      </div>  
+    </div>
+  )
+}
 
 export default Blog
